1e52acc30047ef1b2d995c92b6a84cd93af2b43c
Paul Fitzgerald <paulfitz99@gmail.com>
2015-02-19 19:04:05 +0000

    removed player 2 options

--- app/server.rb
+++ app/server.rb
@@ -5,8 +5,6 @@ require_relative '../lib/board'
 require_relative '../lib/cell'
 require_relative '../lib/ship'
 
-
-
 class BattleShipsWeb < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "..", "views") }
@@ -26,21 +24,17 @@ class BattleShipsWeb < Sinatra::Base
 
   post('/new_game') do
     ship = Ship.new(1)
-    ship2 = Ship.new(1)
     ship3 = Ship.new(1)
-    ship4 = Ship.new(1)
     player = Player.new()
     player.name = params[:user_name]
     player.board = Board.new(Cell)
     player.board.place(ship, :A7)
-    player.board.place(ship2, :B7)
     game.add_player(player)
     computer = Player.new()
     computer.name = "computer"
     computer.board = Board.new(Cell)
     game.add_player(computer)
     computer.board.place(ship3, :A1)
-    computer.board.place(ship4, :B1)
     @player = game.current_player
     erb :new_game
   end
